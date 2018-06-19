ab2e35b71cf9e01b6a26e5c1fc809761717a50a6
apuskarczyk <apuskarczyk@iocean.fr>
2018-06-19 09:44:11 +0200

    up

--- src/main/java/lr_in_the_well/alexis_puska/domain/level/Teleporter.java
+++ src/main/java/lr_in_the_well/alexis_puska/domain/level/Teleporter.java
@@ -17,16 +17,16 @@ public class Teleporter extends Identifiable implements Serializable {
 	private int y;
 	private int length;
 	private boolean vertical;
-	private int toId;
+	private String destinations;
 
-	public Teleporter(int id, boolean enable, int x, int y, int length, boolean vertical, int toId) {
+	public Teleporter(int id, boolean enable, int x, int y, int length, boolean vertical, String destinations) {
 		super(id);
 		this.enable = enable;
 		this.x = x;
 		this.y = y;
 		this.length = length;
 		this.vertical = vertical;
-		this.toId = toId;
+		this.destinations = destinations;
 	}
 
 }
--- src/main/java/lr_in_the_well/alexis_puska/view/properties/TeleporterPanel.java
+++ src/main/java/lr_in_the_well/alexis_puska/view/properties/TeleporterPanel.java
@@ -20,76 +20,91 @@ import lr_in_the_well.alexis_puska.view.IdentifiablePanel;
 
 public class TeleporterPanel extends IdentifiablePanel {
 
-    private static final long serialVersionUID = -4090876979915495722L;
-    
-    private Teleporter teleporter;
+	private static final long serialVersionUID = -4090876979915495722L;
 
-    private JLabel enableLabel;
+	private Teleporter teleporter;
+
+	private JLabel enableLabel;
 	private JCheckBox enableCheckBox;
-    
-    private JLabel destinationLabel;
-    private JTextField destinationTextField;
 
-    public TeleporterPanel(ResourceBundle message, JPanel parent, DrawPanel drawPanel, LevelService levelService, String name,
-            Teleporter teleporter) {
-        super(message, parent, drawPanel, levelService, name);
-        this.teleporter = teleporter;
-        enableLabel = new JLabel(message.getString("properties.teleporter.enable"), JLabel.TRAILING);
+	private JLabel destinationLabel;
+	private JTextField destinationTextField;
+
+	public TeleporterPanel(ResourceBundle message, JPanel parent, DrawPanel drawPanel, LevelService levelService,
+			String name, Teleporter teleporter) {
+		super(message, parent, drawPanel, levelService, name);
+		this.teleporter = teleporter;
+		enableLabel = new JLabel(message.getString("properties.teleporter.enable"), JLabel.TRAILING);
 		enableCheckBox = new JCheckBox();
 		enableLabel.setLabelFor(enableCheckBox);
-        destinationLabel = new JLabel(message.getString("properties.teleporter.destination"), JLabel.TRAILING);
-        destinationTextField = new JTextField();
-        destinationLabel.setLabelFor(destinationTextField);
-        destinationTextField.setToolTipText(message.getString("properties.teleporter.destination.description"));
-        destinationLabel.setToolTipText(message.getString("properties.teleporter.destination.description"));
-        this.add(enableLabel);
+		destinationLabel = new JLabel(message.getString("properties.teleporter.destination"), JLabel.TRAILING);
+		destinationTextField = new JTextField();
+		destinationLabel.setLabelFor(destinationTextField);
+		destinationTextField.setToolTipText(message.getString("properties.teleporter.destination.description"));
+		destinationLabel.setToolTipText(message.getString("properties.teleporter.destination.description"));
+		this.add(enableLabel);
 		this.add(enableCheckBox);
-        this.add(destinationLabel);
-        this.add(destinationTextField);
-        idField.setText(Integer.toString(teleporter.getId()));
-        destinationTextField.setText(Integer.toString(teleporter.getToId()));
-        enableCheckBox.setSelected(teleporter.isEnable());
-        SpringUtilities.makeCompactGrid(this, 3, 2, 6, 6, 6, 6);
-        addListeners();
-        this.parent.updateUI();
-    }
+		this.add(destinationLabel);
+		this.add(destinationTextField);
+		idField.setText(Integer.toString(teleporter.getId()));
+		destinationTextField.setText(teleporter.getDestinations());
+		enableCheckBox.setSelected(teleporter.isEnable());
+		SpringUtilities.makeCompactGrid(this, 3, 2, 6, 6, 6, 6);
+		addListeners();
+		this.parent.updateUI();
+	}
 
-    public void updateTeleporter() {
-        levelService.updateTeleporter(teleporter);
-    }
+	public void updateTeleporter() {
+		levelService.updateTeleporter(teleporter);
+	}
 
-    public void addListeners() {
-        destinationTextField.addCaretListener(new CaretListener() {
-            public void caretUpdate(javax.swing.event.CaretEvent e) {
-                JTextField text = (JTextField) e.getSource();
-                if (text.getText() != null && !text.getText().isEmpty()) {
-                    teleporter.setToId(Integer.valueOf(text.getText()));
-                    levelService.updateTeleporter(teleporter);
-                }
-            }
-        });
-        destinationTextField.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                char vChar = e.getKeyChar();
-                if (!(Character.isDigit(vChar) || (vChar == KeyEvent.VK_BACK_SPACE) || (vChar == KeyEvent.VK_DELETE))) {
-                    e.consume();
-                }
-            }
-            @Override
-            public void keyPressed(KeyEvent e) {
-            }
+	public void addListeners() {
+		destinationTextField.addCaretListener(new CaretListener() {
+			public void caretUpdate(javax.swing.event.CaretEvent e) {
+				JTextField text = (JTextField) e.getSource();
+				if (text.getText() != null && !text.getText().isEmpty()) {
+					String tmp = text.getText();
+					if (tmp.endsWith(",")) {
+						teleporter.setDestinations(tmp.substring(0, tmp.length() - 1));
+					} else {
+						teleporter.setDestinations(tmp);
+					}
+					levelService.updateTeleporter(teleporter);
+				}
+			}
+		});
+		destinationTextField.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char vChar = e.getKeyChar();
+				if ((destinationTextField.getText() != null && !destinationTextField.getText().isEmpty())
+						&& !(destinationTextField.getText().endsWith(","))) {
+					if ((!(Character.isDigit(vChar) || (vChar == KeyEvent.VK_COMMA))
+							|| (vChar == KeyEvent.VK_BACK_SPACE) || (vChar == KeyEvent.VK_DELETE))) {
+						e.consume();
+					}
+				} else {
+					if (!(Character.isDigit(vChar) || (vChar == KeyEvent.VK_BACK_SPACE)
+							|| (vChar == KeyEvent.VK_DELETE))) {
+						e.consume();
+					}
+				}
+			}
 
-            @Override
-            public void keyReleased(KeyEvent e) {
-            }
-        });
-        enableCheckBox.addItemListener(new ItemListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+		});
+		enableCheckBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
 				teleporter.setEnable(enableCheckBox.isSelected());
 				levelService.updateTeleporter(teleporter);
 				drawPanel.repaint();
 			}
 		});
-    }
+	}
 }
